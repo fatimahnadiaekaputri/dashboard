135f8de115e67476b840da352b3e8e49a5f598fe
fatimah nadia <114746979+fatimahnadiaekaputri@users.noreply.github.com>
2023-11-04 12:13:26 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -174,7 +174,7 @@ with col2:
     st.metric("Average Frequency", value=avg_frequency)
  
 with col3:
-    avg_frequency = format_currency(rfm_df.monetary.mean(), "AUD", locale='es_CO') 
+    avg_frequency = format_currency(rfm_df.monetary.mean(), "R$", locale='es_CO') 
     st.metric("Average Monetary", value=avg_frequency)
  
 fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(35, 15))
